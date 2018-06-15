d62319b612eaa07d8b0af0bdaac749a7d84dad10
Binjie Hu <hu_bin_jie24@163.com>
2018-06-16 01:08:41 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,29 +1,29 @@
 ## Download and unzip the zip file data from the website ##
-getwd()
+setwd()
 if(!file.exists("./getdata"))
         dir.create("./getdata")
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl, destfile = "/Users/HBJ/Documents/Coursera/Getting and Cleaning Data/assignment/getdata.zip", method = "curl")
-listzip <- unzip("/Users/HBJ/Documents/Coursera/Getting and Cleaning Data/assignment/getdata.zip", exdir = "/Users/HBJ/Documents/Coursera/Getting and Cleaning Data/assignment")
+download.file(fileUrl, destfile = "~/Documents/Coursera/Getting and Cleaning Data/assignment/getdata.zip", method = "curl")
+listzip <- unzip("~/Documents/Coursera/Getting and Cleaning Data/assignment/getdata.zip", exdir = "~/Documents/Coursera/Getting and Cleaning Data/assignment")
 
 ## Step1. Merges the training and the test sets to create one data set ##
 ## Load the data into R ##
-test_x <- read.table("/Users/HBJ/Documents/Coursera/Getting and Cleaning Data/assignment/UCI HAR Dataset/test/X_test.txt")
+test_x <- read.table("~/Documents/Coursera/Getting and Cleaning Data/assignment/UCI HAR Dataset/test/X_test.txt")
 dim(test_x)
 head(test_x, n=3)
-test_y <- read.table("/Users/HBJ/Documents/Coursera/Getting and Cleaning Data/assignment/UCI HAR Dataset/test/y_test.txt")
+test_y <- read.table("~/Documents/Coursera/Getting and Cleaning Data/assignment/UCI HAR Dataset/test/y_test.txt")
 dim(test_y)
 head(test_y, n=3)
-test_subject <- read.table("/Users/HBJ/Documents/Coursera/Getting and Cleaning Data/assignment/UCI HAR Dataset/test/subject_test.txt")
+test_subject <- read.table("~/Documents/Coursera/Getting and Cleaning Data/assignment/UCI HAR Dataset/test/subject_test.txt")
 dim(test_subject)
 head(test_subject, n=3)
-train_x <- read.table("/Users/HBJ/Documents/Coursera/Getting and Cleaning Data/assignment/UCI HAR Dataset/train/X_train.txt")
+train_x <- read.table("~/Documents/Coursera/Getting and Cleaning Data/assignment/UCI HAR Dataset/train/X_train.txt")
 dim(train_x)
 head(train_x, n=3)
-train_y <- read.table("/Users/HBJ/Documents/Coursera/Getting and Cleaning Data/assignment/UCI HAR Dataset/train/y_train.txt")
+train_y <- read.table("~/Documents/Coursera/Getting and Cleaning Data/assignment/UCI HAR Dataset/train/y_train.txt")
 dim(train_y)
 head(train_y, n=3)
-train_subject <- read.table("/Users/HBJ/Documents/Coursera/Getting and Cleaning Data/assignment/UCI HAR Dataset/train/subject_train.txt")
+train_subject <- read.table("~/Documents/Coursera/Getting and Cleaning Data/assignment/UCI HAR Dataset/train/subject_train.txt")
 dim(train_subject)
 head(train_subject, n=3)
 ## Merge the train and the test data to create a new dataset ##
@@ -36,7 +36,7 @@ dim(Data_subject)
 
 ## Step2. Extracts only the measurements on the mean and standard deviation for each measurement ##
 ## Load the feature names into R ## 
-features <- read.table("/Users/HBJ/Documents/Coursera/Getting and Cleaning Data/assignment/UCI HAR Dataset/features.txt")
+features <- read.table("~/Documents/Coursera/Getting and Cleaning Data/assignment/UCI HAR Dataset/features.txt")
 dim(features)
 ## Extract the mean and standard deviation(std) of each measurements(each row) ##
 meanstd <- grep(("mean\\(\\)|std\\(\\)"), features[, 2])
@@ -47,7 +47,7 @@ dim(Data_x)
 
 ## Step3. Uses descriptive activity names to name the activities in the data set ##
 ## Load the activity labels into R ##
-activity_labels <- read.table("/Users/HBJ/Documents/Coursera/Getting and Cleaning Data/assignment/UCI HAR Dataset/activity_labels.txt")
+activity_labels <- read.table("~/Documents/Coursera/Getting and Cleaning Data/assignment/UCI HAR Dataset/activity_labels.txt")
 dim(activity_labels)
 ## Replace the activities in the data set with descriptive activity names ##
 Data_activity[,1] <- activity_labels[Data_activity[,1], 2]
@@ -74,7 +74,7 @@ library(dplyr)
 groupedData <- group_by(mergedData, Subject, Activity)
 average_Data <- summarise_each(groupedData, funs(mean))
 head(average_Data)
-write.table(average_Data, "/Users/HBJ/Documents/Coursera/Getting and Cleaning Data/assignment/meanData.txt", row.names = FALSE)
+write.table(average_Data, "~/Documents/Coursera/Getting and Cleaning Data/assignment/meanData.txt", row.names = FALSE)
 
 
 
